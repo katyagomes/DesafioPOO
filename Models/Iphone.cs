de43cc4fd178aa769fdb2a5a8e6f8c09c63fc39a
Katya Gomes <73998116+katyagomes@users.noreply.github.com>
2025-12-01 10:47:56 -0300

    Create Iphone.cs

---
+++ Models/Iphone.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no iPhone (iOS)...");
+        }
+    }
+}
